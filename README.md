8de964452afcfc6c4a1eefd9cbf14d1d0153ef57
vzzzz <valeriy.parubets@gmail.com>
2019-03-24 18:38:36 +0700

    Initial and only commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Provides normaliztion of html-formatted documents to plane ASCII text
+
+Usage:
+python3 normalize.py --dir=some_dir_where_html_stored --out=planetext.txt
+or:
+python3 normalize.py --file=some_html_file.html --out=planetext.txt
+('file' key has higher priority than 'dir')
+
+Requirements:
+html2text
---
+++ normalize.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import argparse
+import html2text
+import os
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dir', '-d', help='path to directory with html files', default='.')
+    parser.add_argument('--file', '-f', help='path to certain html file', default=None)
+    parser.add_argument('--out', '-o', help='filename to resulted plane text file', default='normalized.txt')
+    args = parser.parse_args()
+    
+    files = []
+    if args.file is not None:
+        files.append(args.file)
+    else:
+        files = os.listdir(args.dir)
+
+    normalized_text = ''
+    
+    for f in files:
+        if os.path.isfile(f) and f.endswith('html'):
+            html = open(f).read()
+            text = html2text.html2text(html).lower()
+            normalized_text += text
+
+    with open(args.out, 'w+') as normalized_file:
+        normalized_file.write(normalized_text)
